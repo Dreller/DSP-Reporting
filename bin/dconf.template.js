3675402aecc554d0dd1d8304a0a80a02b00dba8c
Dreller <dreller2034@gmail.com>
2023-10-19 07:44:55 -0400

    Update dconf.template.js

--- bin/dconf.template.js
+++ bin/dconf.template.js
@@ -6,16 +6,22 @@
 var _URL = "https://123.sharepoint.com/sites/Mysite";
 
 // List for saving Reports
-var _REPORTLIST = "Reports";
+var _REPORT_LIST = "Reports";
 
-// Allow reporting on Libraries
-var _ALLOW_CUST_LIBRARY = false;
+// Allow reporting on Libraries.
+var _ALLOW_LIBRARY = false;
 
-// Allow reporting on System Libraries
-var _ALLOW_SYST_LIBRARY = false;
+// Allow reporting on Hidden Libraries
+var _ALLOW_LIBRARY_HIDDEN = false;
 
-// Allow reporting on Lists
-var _ALLOW_CUST_LISTS = true;
+// Allow reporting on Lists.
+var _ALLOW_LIST = true;
 
-// Allow using System Fields
-var _ALLOW_SYST_FIELD = false;
+// Allow reporting on Hidden Lists
+var _ALLOW_LIST_HIDDEN = false;
+
+// Always-available Fields (columns) -> Array of "Name"
+var _ALLOW_FIELDS_ALWAYS = ["ID"];
+
+var _SPREPORTIFY_BUILDER_PAGE = "dbr.aspx";
+var _SPREPORTIFY_RUNNER_PAGE = "drr.aspx";
